f723f6026dfc49cba7e639903409a2620d97963c
John Q Developer <202678382+JohnQDeveloper@users.noreply.github.com>
2025-04-22 22:47:55 -0400

    current state of us and some contrib tweaks

--- bunny-upload.js
+++ bunny-upload.js
@@ -2,12 +2,13 @@ import uploadToBunny from 'upload-to-bunny';
 
 import fs from 'node:fs';
 
-const api_key = fs.readFileSync('./bunny-api-key.txt', 'utf8');
+const upload_key = fs.readFileSync('./bunny-api-key.txt', 'utf8');
+const api_key = fs.readFileSync('./bunny-rest-api-key.txt', 'utf8');
 
 await uploadToBunny('./public', '', {
   storageZoneName: 'jqdeveloper-com',
   cleanDestination: true,
-  accessKey: api_key,
+  accessKey: upload_key,
   maxConcurrentUploads: 10,
   region: ''
 });
@@ -16,7 +17,15 @@ const url = 'https://api.bunny.net/pullzone/3531648/purgeCache';
 const options = {method: 'POST', headers: {'content-type': 'application/json', 'AccessKey': api_key}};
 
 fetch(url, options)
-  .then(res => res.json())
+  .then(function(response) {
+    console.log(response.status); // Will show you the status
+    if (response.status == '204') {
+        console.log("Cache was purged per API spec (HTTP 204)");
+        return;
+    }
+
+    res => res.json()
+  })
   .then(json => console.log(json))
   .catch(err => console.error(err));
 
